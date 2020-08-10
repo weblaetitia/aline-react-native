38f8701b0fb703b489504a1968a45cae07e47bed
CorTrt <trt.corentin@gmail.com>
2020-08-10 14:19:02 +0200

    Font > moreInfo + Intégration favScreen

--- screens/fav.js
+++ screens/fav.js
@@ -1,23 +1,142 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
+import { Card } from 'react-native-elements';
+
+import { AppLoading } from 'expo';
+import { useFonts, Capriola_400Regular } from '@expo-google-fonts/capriola';
+
 import { StatusBar } from 'expo-status-bar';
 
+
+/* Color ref */
+var blueDark = '#033C47';
+var mint = '#2DB08C';
+
+
 function FavScreen() {
+
+  const favList = [
+    {
+      title: "Bien l'épicerie",
+      adress: '123 rue Réaumur -',
+      zipCode: '75002',
+      city: 'Paris',
+      description: "Magasin d'alimentation bio",
+      services: 'Bouteilles consignées',
+      type: 'boutique'
+    },
+    {
+      title: "Bien le réstaurant",
+      adress: '321 rue Réaumur -',
+      zipCode: '75002',
+      city: 'Paris',
+      description: "Restaurant veg freindly",
+      services: 'Boites consignées',
+      type: 'restaurant'
+    },
+  ]
+
+  var favListGroup = favList.map((fav,i)=> {
+
+    
+    return(
+
+        <Card
+          key= {i}
+          containerStyle = {styles.card} >
+            <View style = {styles.cardHead} >
+                <View style = {styles.cardTitle} >
+                    <Image
+                      style = {{width: '13%'}}
+                      resizeMode ='contain'
+                      source = {
+                        fav.type == 'boutique' ? require('../assets/icons/boutique.png') :
+                        fav.type == 'restaurant' ? require('../assets/icons/restaurant.png') :
+                        require('../assets/icons/heart.png')
+                      } 
+                      />
+                    <Text style = {styles.h1Card}>
+                      {fav.title}
+                    </Text>
+                </View>
+
+                <Image
+                      style = {{width: '9%'}}
+                      resizeMode = 'contain'
+                      source = {require('../assets/icons/heart.png')} />
+            </View>
+
+            <View style = {styles.cardAdress} >
+                <Text style = {{color: blueDark, marginBottom: 10}} >
+                  {fav.adress}
+                </Text>
+                <Text style = {{color: blueDark, marginBottom: 10, marginLeft: 5}} >
+                  {fav.zipCode}
+                </Text>
+                <Text style = {{color: blueDark, marginBottom: 10, marginLeft: 5}} >
+                  {fav.city}
+                </Text>
+            </View>
+        
+            <Text style = {{color: blueDark, marginBottom: 5}} >
+              {fav.description}
+            </Text>
+            <Text style = {{color: blueDark, marginBottom: 10}} >
+              {fav.services}
+            </Text>
+
+        </Card>
+
+    )
+  })
+
+  let [fontsLoaded] = useFonts({Capriola_400Regular,})
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+
     return (
-      <View style={styles.container}>
-        <Text>Hi this is FAV!</Text>
-        <StatusBar style="auto" />
-      </View>
+      <ScrollView>
+
+        {favListGroup}
+
+      </ScrollView>
     );
+
   }
+
+}
   
   const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
+
+    card: {
+      width: '100%',
+      paddingHorizontal: 25,
+      paddingVertical: 10,
+      margin: 0 
     },
+    cardHead: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'space-between'
+    },
+    cardTitle: {
+      flexDirection: 'row',
+      alignItems: 'center'
+    },
+    h1Card: {
+      color: mint,
+      fontFamily: 'Capriola_400Regular',
+      fontSize: 18,
+      marginLeft: 10
+    },
+    cardAdress: {
+      flexDirection: 'row',
+      marginBottom: 10
+    }
+
   });
 
 
--- screens/moreInfo.js
+++ screens/moreInfo.js
@@ -8,158 +8,171 @@ import { AntDesign } from '@expo/vector-icons';
 import { FontAwesome5 } from '@expo/vector-icons'; 
 import { MaterialCommunityIcons } from '@expo/vector-icons'; 
 
+import { AppLoading } from 'expo';
+import { useFonts, Capriola_400Regular } from '@expo-google-fonts/capriola';
+
+
 /* Color ref */
 var blueDark = '#033C47';
 var mint = '#2DB08C';
 
 
 function MoreInfoScreen() {
-    return (
-        <ScrollView>
 
-          {/* ALINE ET MOI */}
-          <ListItem
-                titleStyle={{color: mint, fontWeight:'bold'}}
-                title='Aline et moi'
-                bottomDivider
-          />
-          <ListItem
+  let [fontsLoaded] = useFonts({Capriola_400Regular,}) 
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+
+          <ScrollView>
+
+            {/* ALINE ET MOI */}
+            <ListItem
+                  titleStyle={styles.h1}
+                  title='Aline et moi'
+                  bottomDivider
+            />
+            <ListItem
+              rightIcon={
+                  <FontAwesome
+                    name='user'
+                    size={20}
+                    color= {mint}
+                  />
+              }
+              titleStyle={{color:'#033C47'}}
+              title='Mon compte'
+              bottomDivider
+            />
+            <ListItem
+              rightIcon={
+                  <AntDesign
+                    name='logout'
+                    size={20}
+                    color= {mint}
+                  />
+              }
+              titleStyle={{color:'#033C47'}}
+              title='Logout'
+              bottomDivider
+            />
+            <ListItem
+              rightIcon={
+                  <FontAwesome5
+                    name='coins'
+                    size={20}
+                    color= {mint}
+                  />
+              }
+              titleStyle={{color:'#033C47'}}            
+              title='Soutenir Aline'
+              bottomDivider
+            />
+            <ListItem
+            rightIcon={
+                <MaterialCommunityIcons
+                  name='map-marker-plus'
+                  size={20}
+                  color= {mint}
+                />
+            }  
+            titleStyle={{color:'#033C47'}}
+            title='Nourir Aline'
+            bottomDivider
+            />
+
+            {/* RESTONS EN CONTACT */}
+            <ListItem
+              containerStyle={{paddingTop:30}}
+              titleStyle={styles.h1}
+              title='Restons en contact'
+              bottomDivider
+            />
+            <ListItem
             rightIcon={
                 <FontAwesome
-                  name='user'
+                  name='facebook-f'
                   size={20}
-                  color= {blueDark}
+                  color= {mint}
                 />
-            }
+            }  
             titleStyle={{color:'#033C47'}}
-            title='Mon compte'
+            title='Facebook'
             bottomDivider
-          />
-          <ListItem
+            />
+            <ListItem
             rightIcon={
                 <AntDesign
-                  name='logout'
+                  name='instagram'
                   size={20}
-                  color= {blueDark}
+                  color= {mint}
+                />
+            } 
+            titleStyle={{color:'#033C47'}} 
+            title='Instagram'
+            bottomDivider
+            />
+            <ListItem
+            rightIcon={
+                <AntDesign
+                  name='twitter'
+                  size={20}
+                  color= {mint}
                 />
             }
             titleStyle={{color:'#033C47'}}
-            title='Logout'
+            title='Twitter'
             bottomDivider
-          />
-          <ListItem
+            />
+            <ListItem
             rightIcon={
                 <FontAwesome5
-                  name='coins'
+                  name='desktop'
                   size={20}
-                  color= {blueDark}
+                  color= {mint}
                 />
-            }
-            titleStyle={{color:'#033C47'}}            
-            title='Soutenir Aline'
+            } 
+            titleStyle={{color:'#033C47'}} 
+            title='Site internet'
             bottomDivider
-          />
-          <ListItem
-          rightIcon={
-              <MaterialCommunityIcons
-                name='map-marker-plus'
-                size={20}
-                color= {blueDark}
-              />
-          }  
-          titleStyle={{color:'#033C47'}}
-          title='Nourir Aline'
-          bottomDivider
-          />
+            />
+            <ListItem
+            rightIcon={
+                <FontAwesome
+                  name='share-alt'
+                  size={20}
+                  color= {mint}
+                />
+            } 
+            titleStyle={{color:'#033C47'}} 
+            title='Partager cet application'
+            bottomDivider
+            />
 
-          {/* RESTONS EN CONTACT */}
-          <ListItem
-            containerStyle={{paddingTop:30}}
-            titleStyle={{color: mint, fontWeight:'bold'}}
-            title='Restons en contact'
-            bottomDivider
-          />
-          <ListItem
-          rightIcon={
-              <FontAwesome
-                name='facebook-f'
-                size={20}
-                color= {blueDark}
-              />
-          }  
-          titleStyle={{color:'#033C47'}}
-          title='Facebook'
-          bottomDivider
-          />
-          <ListItem
-          rightIcon={
-              <AntDesign
-                name='instagram'
-                size={20}
-                color= {blueDark}
-              />
-          } 
-          titleStyle={{color:'#033C47'}} 
-          title='Instagram'
-          bottomDivider
-          />
-          <ListItem
-          rightIcon={
-              <AntDesign
-                name='twitter'
-                size={20}
-                color= {blueDark}
-              />
-          }
-          titleStyle={{color:'#033C47'}}
-          title='Twitter'
-          bottomDivider
-          />
-          <ListItem
-          rightIcon={
-              <FontAwesome5
-                name='desktop'
-                size={20}
-                color= {blueDark}
-              />
-          } 
-          titleStyle={{color:'#033C47'}} 
-          title='Site internet'
-          bottomDivider
-          />
-          <ListItem
-          rightIcon={
-              <FontAwesome
-                name='share-alt'
-                size={20}
-                color= {blueDark}
-              />
-          } 
-          titleStyle={{color:'#033C47'}} 
-          title='Partager cet application'
-          bottomDivider
-          />
+            {/* INFORMATIONS */}
+            <ListItem
+              containerStyle={{paddingTop:30}}
+              titleStyle={styles.h1}
+              title='Informations'
+              bottomDivider
+            />
+            <ListItem
+              titleStyle={{color: blueDark}}
+              title='Mentions légales'
+              bottomDivider
+            />
+            <ListItem 
+              titleStyle={{color: blueDark}}
+              title='CGU'
+              bottomDivider
+            />
 
-          {/* INFORMATIONS */}
-          <ListItem
-            containerStyle={{paddingTop:30}}
-            titleStyle={{color: mint, fontWeight:'bold'}}
-            title='Informations'
-            bottomDivider
-          />
-          <ListItem
-            titleStyle={{color: blueDark}}
-            title='Mentions légales'
-            bottomDivider
-          />
-          <ListItem 
-            titleStyle={{color: blueDark}}
-            title='CGU'
-            bottomDivider
-          />
+          </ScrollView>
+      );
 
-        </ScrollView>
-    );
+    }
+
   }
   
   const styles = StyleSheet.create({
@@ -168,6 +181,11 @@ function MoreInfoScreen() {
       backgroundColor: '#fff',
       alignItems: 'center',
       justifyContent: 'center',
+    },
+    h1: {
+      fontFamily: 'Capriola_400Regular',
+      color: mint,
+      fontSize: 18
     },
   });
 
