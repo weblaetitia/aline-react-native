d2736e941ec953e0f4c376209a3a828dfd42a269
CorTrt <trt.corentin@gmail.com>
2020-08-13 16:32:55 +0200

    Affichage des markers

--- screens/mapExplorer.js
+++ screens/mapExplorer.js
@@ -1,16 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { StyleSheet, Dimensions} from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
 
 
 function MapScreen(props) {
 
+  const [placesMarkers, setPlacesMarkers] = useState([])
+
+  useEffect(() => {   
+    
+    var networkFromFront = 'Reconcil'
+
+      async function getPlaces (data) {
+
+          var response = await fetch('http://10.2.3.30:3000/map/getPlaces', {
+            method: 'POST',
+            headers: {'Content-type': 'application/x-www-form-urlencoded'},
+            body: 'networkFromFront=Reconcil',
+          })
+          var rawResponse = await response.json();  
+          setPlacesMarkers(rawResponse)
+
+      }
+      getPlaces()
+
+  }, []);
+
+
+  var placesMarkersList = placesMarkers.map((place,i)=> {
+    return(<Marker
+            coordinate={{latitude: place.latitude, longitude: place.longitude}}
+            title={place.name}
+            description={place.type}
+          />)
+    })
+
 
   return (
     
-        
-        <MapView style = {styles.mapStyle} />
+        <MapView style = {styles.mapStyle}>
+
+            {placesMarkersList}
+
+        </MapView>
 
     );
   }
