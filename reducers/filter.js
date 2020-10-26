aab9884d1d81048bea91620e853b795e084c68ce
Laetitia Langlois <weblaetitia@hotmail.com>
2020-10-26 18:13:30 +0100

    replaced filter modal by a modal-screen

--- reducers/filter.js
+++ reducers/filter.js
@@ -1,6 +1,7 @@
 export default function(filterDatas = {}, action) {
-    if (action.type === 'saveData') {
-        return action.data
+    if (action.type === 'saveFilterData') {
+        console.log('Mes filtres enregistrés : ', action.filterDatas)
+        return action.filterDatas
     } else  {
         return filterDatas
     }
