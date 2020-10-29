01e761abf720e90b9890a640797e9cf6e8ba6a2c
Laetitia Langlois <weblaetitia@hotmail.com>
2020-10-29 11:22:59 +0100

    change apple map for googlemap on iOS

--- components/map.js
+++ components/map.js
@@ -1,15 +1,35 @@
 import React, { useState } from 'react';
 import { StyleSheet, Dimensions, Text, View, Image} from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 
 // my components
 import MapModal from '../components/mapModal';
+
+// map style
+const mapStyle = [
+    {
+      "featureType": "poi",
+      "elementType": "labels",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    }
+  ]
 
 
 function Map(props) {
 
     const [modalVisibility, setModalVisibility] = useState(false);
     const [markerSelected, setMarkerSelected] = useState(null);
+
+    const [region, setRegion] = useState({
+        latitude: 44.74925167481967,
+        longitude: 5.3769888509293935,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+    })
 
     const displayModal = (place) => {
         setModalVisibility(true)
@@ -19,30 +39,38 @@ function Map(props) {
     }
 
     // set markers list
-    let placesMarkersList = props.filteredPlaces.map((place,i) => {
-        return(
-            <Marker
-              key={`marker${i}`}
-              coordinate={{latitude: place.latitude, longitude: place.longitude}}
-              onSelect={ ()=> { displayModal(place), setMarkerSelected(i) } }
-              onDeselect={ () => { hideModal(), setMarkerSelected('') } }
-            >
-                <Image
-                  source={place.type == 'shop' ? require('../assets/icons/markerBoutique.png') : require('../assets/icons/markerRestaurant.png')}
-                  style={markerSelected === i ?{width: 30} : {width: 18} }
-                  resizeMode='contain'
-                />
-            </Marker>
-        )
-    })
+    const smallViewBox = '0 0 22 30'
+    const largeViewBox = '0 0 30 41'
 
     return (
         <View style={{ flex: 1 }}>
-            <MapView style={styles.mapStyle}
+            <MapView 
+                provider={PROVIDER_GOOGLE}
+                style={styles.mapStyle}
                 rotateEnabled={false}
-                region={props.region}
+                region={region}
+                onRegionChangeComplete={region => setRegion(region)}
+                showsTraffic={false}
+                loadingEnabled={true}
+                customMapStyle={mapStyle}
             >
-                {placesMarkersList} 
+                { props.filteredPlaces.map((place,i) => {
+                        return(
+                            <Marker
+                              key={`marker${i}`}
+                              coordinate={{latitude: place.latitude, longitude: place.longitude}}
+                              onPress={ ()=> { displayModal(place), setMarkerSelected(i) } }
+                              onDeselect={ () => { hideModal(), setMarkerSelected('') } }
+                            >
+                              <Image
+                                source={place.type == 'shop' ? require('../assets/icons/markerBoutique.png') : require('../assets/icons/markerRestaurant.png')}
+                                style={markerSelected === i ?{width: 36} : {width: 24} }
+                                resizeMode='contain'
+                              />
+                            </Marker>
+                        )
+                    })
+                    } 
                 <Marker 
                     coordinate={{latitude:props.userPosition.currentLat, longitude:props.userPosition.currentLong}}
                     title='Moi'
