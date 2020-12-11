751e579afed66a9f704c9680db5ef2701e5ac2c6
Laetitia Langlois <weblaetitia@hotmail.com>
2020-12-11 13:26:31 +0100

    fix(ui): update 'more-info' screen with actual informations

--- screens/moreInfo.js
+++ screens/moreInfo.js
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
-import { AsyncStorage, StyleSheet, View, ScrollView, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, ScrollView, TouchableOpacity, Text, Image } from 'react-native';
 import { ListItem } from 'react-native-elements';
 
 import { FontAwesome } from '@expo/vector-icons';
@@ -10,7 +10,6 @@ import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 import { AppLoading } from 'expo';
 import { useFonts, Capriola_400Regular } from '@expo-google-fonts/capriola';
-import token from '../reducers/token';
 import { connect } from 'react-redux';
 
 import * as Linking from 'expo-linking';
@@ -78,7 +77,7 @@ function MoreInfoScreen(props) {
     return <AppLoading />;
   } else {
     return (
-      <ScrollView>
+      <ScrollView style={{backgroundColor: 'white'}}>
 
             {/* ALINE ET MOI */}
             <ListItem
@@ -89,21 +88,24 @@ function MoreInfoScreen(props) {
             {/* mon compte || se connecter */}
             {lignes}
 
-            <ListItem
-              rightIcon={
-                  <FontAwesome5
-                    name='coins'
-                    size={20}
-                    color= {mint}
-                  />
-              }
-              titleStyle={{color:'#033C47'}}            
-              title='Soutenir Aline'
-              bottomDivider
-            />
+            {/* soutenir Aline */}
+            <TouchableOpacity onPress={() => Linking.openURL('https://fr.tipeee.com/alineconsigne/')}>
+              <ListItem
+                rightIcon={
+                    <FontAwesome5
+                      name='coins'
+                      size={20}
+                      color= {mint}
+                    />
+                }
+                titleStyle={{color:'#033C47'}}            
+                title='Soutenir Aline avec un tip'
+                bottomDivider
+              />
+            </TouchableOpacity>
 
             {/* nourrir Aline */}
-            <TouchableOpacity onPress={() => Linking.openURL('https://aline-consigne.herokuapp.com')} >
+            <TouchableOpacity onPress={() => Linking.openURL('https://aline.app')}>
             <ListItem
             rightIcon={
                 <MaterialCommunityIcons
@@ -118,28 +120,48 @@ function MoreInfoScreen(props) {
             />
             </TouchableOpacity>
 
+            {/* Nous écrire */}
+            <TouchableOpacity onPress={() => Linking.openURL('mailto:contact@aline.app')}>
+            <ListItem
+            rightIcon={
+              <FontAwesome5
+              name='envelope'
+              size={20}
+              color= {mint}
+            />
+            }  
+            titleStyle={{color:'#033C47'}}
+            title='Nous écrire'
+            bottomDivider
+            />
+            </TouchableOpacity>
+
             {/* RESTONS EN CONTACT */}
             <ListItem
               containerStyle={{paddingTop:30}}
               titleStyle={styles.h1}
               title='Restons en contact'
               bottomDivider
-            />
-            <ListItem
-            rightIcon={
-                <FontAwesome
-                  name='facebook-f'
-                  size={20}
-                  color= {mint}
-                />
-            }  
-            titleStyle={{color:'#033C47'}}
-            title='Facebook'
-            bottomDivider
-            />
+              />
+
+            {/* Facebook */}
+            <TouchableOpacity onPress={() => Linking.openURL('https://www.facebook.com/alineconsigne')}>
+              <ListItem
+              rightIcon={
+                  <FontAwesome
+                    name='facebook-f'
+                    size={20}
+                    color= {mint}
+                  />
+              }  
+              titleStyle={{color:'#033C47'}}
+              title='Facebook'
+              bottomDivider
+              />
+            </TouchableOpacity>
 
             {/* instagram */}
-            <TouchableOpacity onPress={() => Linking.openURL('https://www.instagram.com/alineconsigne/')} >
+            <TouchableOpacity onPress={() => Linking.openURL('https://www.instagram.com/alineconsigne/')}>
             <ListItem
             rightIcon={
                 <AntDesign
@@ -156,7 +178,7 @@ function MoreInfoScreen(props) {
             </TouchableOpacity>
 
             {/* twitter */}
-            <TouchableOpacity onPress={() => Linking.openURL('https://twitter.com/aline_consigne')} >
+            <TouchableOpacity onPress={() => Linking.openURL('https://twitter.com/aline_consigne')}>
             <ListItem
             rightIcon={
                 <AntDesign
@@ -171,48 +193,31 @@ function MoreInfoScreen(props) {
             />
             </TouchableOpacity>
             
-            <ListItem
-            rightIcon={
-                <FontAwesome5
-                  name='desktop'
-                  size={20}
-                  color= {mint}
-                />
-            } 
-            titleStyle={{color:'#033C47'}} 
-            title='Site internet'
-            bottomDivider
-            />
-            <ListItem
-            rightIcon={
-                <FontAwesome
-                  name='share-alt'
-                  size={20}
-                  color= {mint}
-                />
-            } 
-            titleStyle={{color:'#033C47'}} 
-            title='Partager cet application'
-            bottomDivider
-            />
+            {/* web */}
+            <TouchableOpacity onPress={() => Linking.openURL('https://aline.app')}>
+              <ListItem
+              rightIcon={
+                  <FontAwesome5
+                    name='desktop'
+                    size={20}
+                    color= {mint}
+                  />
+              } 
+              titleStyle={{color:'#033C47'}} 
+              title='Site internet'
+              bottomDivider
+              />
+            </TouchableOpacity>
 
-            {/* INFORMATIONS */}
-            <ListItem
-              containerStyle={{paddingTop:30}}
-              titleStyle={styles.h1}
-              title='Informations'
-              bottomDivider
-            />
-            <ListItem
-              titleStyle={{color: blueDark}}
-              title='Mentions légales'
-              bottomDivider
-            />
-            <ListItem 
-              titleStyle={{color: blueDark}}
-              title='CGU'
-              bottomDivider
-            />
+
+            <View style={{marginVertical:30, justifyContent: 'center', alignItems: 'center'}}>
+            <Image style={{height: 38, width: 110, marginBottom: 20}} source={require('../assets/images/logo.png')} />
+              <Text style={{color:'#033C47', marginBottom: 20}}>Version 1.0.0</Text>
+              <Text style={{color:'#033C47', marginBottom: 20}}>© Aline 2020 tout droits réservés</Text>
+              <TouchableOpacity onPress={() => Linking.openURL('https://aline.app/app-cgu')}>
+                <Text style={{color:'#033C47', textDecorationLine: 'underline', fontSize: 12}}>Conditions générales d'utilisation</Text>
+              </TouchableOpacity>
+            </View>
 
           </ScrollView>
       );
